e50048274c7b33f7560c366e7276799c29b845e7
Eduardo Diaz Ugarte <diaz268@users.noreply.github.com>
2022-05-03 22:25:38 -0500

    feat: add icons

---
+++ src/components/icon/Pokebola.vue
@@ -0,0 +1,46 @@
+<template>
+  <svg width="106" height="106" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g transform="translate(-523 -297)">
+      <path
+        style="
+          font-variation-settings: normal;
+          opacity: 1;
+          fill: #f22539;
+          fill-opacity: 1;
+          stroke-width: 4;
+          stroke-linecap: butt;
+          stroke-linejoin: miter;
+          stroke-miterlimit: 4;
+          stroke-dasharray: none;
+          stroke-dashoffset: 0;
+          stroke-opacity: 1;
+          stop-color: #000;
+          stop-opacity: 1;
+        "
+        d="M576 299a51 51 0 0 0-51 51 51 51 0 0 0 .002.016h30.193a20.807 20.807 0 0 1-.002-.016A20.807 20.807 0 0 1 576 329.193 20.807 20.807 0 0 1 596.807 350a20.807 20.807 0 0 1-.002.016h30.193A51 51 0 0 0 627 350a51 51 0 0 0-51-51Z" />
+      <path
+        style="
+          font-variation-settings: normal;
+          vector-effect: none;
+          fill: #fff;
+          fill-opacity: 1;
+          stroke-width: 4;
+          stroke-linecap: butt;
+          stroke-linejoin: miter;
+          stroke-miterlimit: 4;
+          stroke-dasharray: none;
+          stroke-dashoffset: 0;
+          stroke-opacity: 1;
+          stop-color: #000;
+        "
+        d="M576 401c-28.167 0-51-22.833-51-51l.002-.016h30.193l-.002.016c0 11.491 3.064-18.832 20.807-18.832s20.806 30.323 20.807 18.832l-.002-.016h30.193L627 350c0 28.167-22.833 51-51 51z" />
+      <path
+        fill="#333"
+        d="M576 297c-29.247 0-53 23.753-53 53s23.753 53 53 53 53-23.753 53-53-23.753-53-53-53zm0 4c26.415 0 47.9 20.845 48.95 47h-28.241c-1.011-10.534-9.916-18.807-20.709-18.807-10.39 0-19.032 7.666-20.564 17.637h-28.323C528.746 321.227 549.981 301 576 301Zm0 32.193c9.306 0 16.807 7.5 16.807 16.807 0 9.306-7.5 16.807-16.807 16.807-9.306 0-16.807-7.5-16.807-16.807 0-9.306 7.5-16.807 16.807-16.807zm-48.979 17.637h28.194c.438 11.085 9.596 19.977 20.785 19.977 10.793 0 19.698-8.273 20.709-18.807h28.24c-1.048 26.155-22.534 47-48.949 47-26.807 0-48.534-21.47-48.979-48.17z" />
+      <path
+        fill="#fff"
+        d="M588.955 307.932a2 2 0 0 0-2.5 1.324 2 2 0 0 0 1.324 2.498c11.344 3.489 20.548 11.9 25.11 22.73a2 2 0 0 0 2.619 1.067 2 2 0 0 0 1.066-2.62c-5.02-11.92-15.131-21.158-27.619-25z" />
+      <circle cx="576" cy="350" r="10.385" stroke="gray" stroke-width="2" />
+    </g>
+  </svg>
+</template>
